0159387c5b19209966d169c7d1a9d00003e924ae
Kayque Moreira <kayque.moreira@cepedi.org.br>
2023-10-23 00:49:56 -0300

    first commit

---
+++ client.py
@@ -0,0 +1,84 @@
+import xmlrpc.client
+
+# Função para imprimir o tabuleiro
+def print_tabuleiro(tab):
+    for row in tab:
+        for cell in row:
+            print(f"[{' ' if cell == 0 else ('x' if cell == 2 else 'o') }]", end='')
+        print()
+
+tab1 = [[0] * 6 for _ in range(6)]
+tab2 = [[0] * 6 for _ in range(6)]
+
+# Variáveis para controlar a pontuação dos jogadores
+pontuacao_jogador1 = 0
+pontuacao_jogador2 = 0
+
+with xmlrpc.client.ServerProxy("http://127.0.0.1:8000/") as cli:
+    id = cli.logar()
+
+    print("Bem-vindo ao Jogo de Batalha Naval!")
+    print(f"Você é o Jogador {id}. Posicione seus 3 barcos.")
+
+    for _ in range(3):
+        while True:
+            vx, vy = map(int, input("Insira as coordenadas de posicionamento (x y): ").split())
+            # Subtrair 1 das coordenadas para corresponder à matriz
+            vx -= 1
+            vy -= 1
+            if 0 <= vx <= 5 and 0 <= vy <= 5:
+                if tab1[vx][vy] == 0:
+                    break
+                else:
+                    print("Você já posicionou um barco nessa posição. Escolha outra.")
+            else:
+                print("Coordenadas fora dos limites. Escolha valores entre 1 e 6.")
+        cli.positionar(id, vx, vy)
+        # Atualize a matriz tab1 com 'x' nas posições dos barcos
+        tab1[vx][vy] = 'x'
+
+    print("Tabuleiro após posicionar os barcos:")
+    print_tabuleiro(tab1)
+
+while True:
+    gg = cli.ganhador()
+    if gg != "-":
+        break
+
+    # Imprima a pontuação dos jogadores a cada atualização
+    print(f"Pontuação - Jogador 1: {pontuacao_jogador1}, Jogador 2: {pontuacao_jogador2}")
+
+    # Verifique de quem é o turno
+    if id == 1 and gg == "-":
+        jogador = "Jogador 1"
+    elif id == 2 and gg == "-":
+        jogador = "Jogador 2"
+    else:
+        jogador = "Jogador desconhecido"
+
+    if gg == "-":
+        ataque_p = input(f"{jogador}, insira as coordenadas de ataque (x y): ").split()
+        if len(ataque_p) == 2 and all(coord.isdigit() for coord in ataque_p):
+            ataque_p = list(map(int, ataque_p))
+            if 1 <= ataque_p[0] <= 6 and 1 <= ataque_p[1] <= 6:
+                atk = cli.atacar(id, ataque_p[0], ataque_p[1])
+                print("Valor retornado da função atacar:", atk)
+                if atk == '1':
+                    if jogador == "Jogador 1":
+                        pontuacao_jogador1 += 1
+                        print("Você acertou um barco inimigo!")
+                        tab2[ataque_p[0] - 1][ataque_p[1] - 1] = 'x'
+                    else:
+                        pontuacao_jogador2 += 1
+                        print("Você acertou um barco inimigo!")
+                        tab1[ataque_p[0] - 1][ataque_p[1] - 1] = 'x'
+                elif atk == '2':
+                    print("Você acertou uma posição já atacada.")
+                else:
+                    print("Seu ataque errou!")
+            else:
+                print("Coordenadas de ataque fora dos limites. Escolha valores entre 1 e 6.")
+        else:
+            print("Coordenadas de ataque inválidas. Insira duas coordenadas separadas por espaço.")
+
+print(gg)
---
+++ server.py
@@ -0,0 +1,84 @@
+from xmlrpc.server import SimpleXMLRPCServer
+
+class Users:
+    def __init__(self):
+        self.id = 0
+        self.login = ""
+        self.senha = ""
+        self.ponto = 0
+
+users = [Users(), Users()]
+ganhador = 0
+
+tab1 = [[0] * 6 for _ in range(6)]
+tab2 = [[0] * 6 for _ in range(6)]
+
+def login():
+    global ganhador
+    if users[0].id == 0:
+        users[0].id = 1
+        return 1
+    else:
+        users[1].id = 2
+        return 2
+
+def positionar(id, px, py):
+    if id == 1:
+        tab1[px][py] = 1
+    else:
+        tab2[px][py] = 1
+    return 1
+
+def atacar(id, px, py):
+    global ganhador
+    if ganhador != 0:
+        return str(ganhador)  # Converta para uma string
+
+    if px < 0 or px >= 6 or py < 0 or py >= 6:
+        return "0"  # Retorna "0" para indicar que o ataque está fora dos limites
+
+    if id == 1:
+        if tab2[px][py] == 1:  # Verifica se há um barco na tabela do Jogador 2
+            if tab2[px][py] != 2:  # Verifica se a posição não foi atacada antes
+                tab2[px][py] = 2  # Marque a posição como atacada
+                users[0].ponto += 1
+                if users[0].ponto == 1:
+                    ganhador = 1
+                return "1"  # Retorne "1" para indicar que o ataque foi bem-sucedido
+    elif id == 2:
+        if tab1[px][py] == 1:  # Verifica se há um barco na tabela do Jogador 1
+            if tab1[px][py] != 2:  # Verifica se a posição não foi atacada antes
+                tab1[px][py] = 2  # Marque a posição como atacada
+                users[1].ponto += 1
+                if users[1].ponto == 1:
+                    ganhador = 2
+                return "1"  # Retorne "1" para indicar que o ataque foi bem-sucedido
+
+    return "0"  # Retorna "0" para indicar que o ataque falhou
+
+
+
+def get_tabuleiro(id):
+    return tab1 if id == 1 else tab2
+
+def verificar_ganhador():
+    global ganhador
+    if ganhador == 1:
+        return "Jogador 1 ganhou"
+    elif ganhador == 2:
+        return "Jogador 2 ganhou"
+    return "-"
+
+
+with SimpleXMLRPCServer(('127.0.0.1', 8000)) as server:
+    server.register_function(login, "logar")
+    server.register_function(positionar, "positionar")
+    server.register_function(atacar, "atacar")
+    server.register_function(get_tabuleiro, "get_tabuleiro")
+    server.register_function(verificar_ganhador, "ganhador")
+
+
+    print("Servidor da Batalha Naval em execução.")
+    print("Aguardando conexões de jogadores...")
+
+    server.serve_forever()
